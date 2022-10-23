6aa0302d7ae168b7f5784742445ae742980ead14
Benedict Hazel <bwhazel@gmail.com>
2022-10-23 10:51:51 +0100

    Add basic GET method to get all comics in the database.

--- api/src/app.mjs
+++ api/src/app.mjs
@@ -1,10 +1,23 @@
 import express from 'express';
 
+import gremlinClient from './data/gremlinClient.mjs';
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.status(200)
         .send('Hello, World!');
+});
+
+app.get('/comics', async(req, res) => {
+    try {
+        const comics = await gremlinClient.submit('g.V()', {});
+        res.status(200)
+            .send(comics);
+    } catch (error) {
+        res.status(400)
+            .send(error);
+    }
 });
 
 app.listen(8080, () => {
