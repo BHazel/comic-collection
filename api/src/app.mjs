06c9f4934b0ea8068be7205d20e4562fc1bd439b
Benedict Hazel <bwhazel@gmail.com>
2022-10-23 22:21:20 +0100

    Clean up main comics endpoint and add new endpoints to retrieve a comic and its sequels.

--- api/src/app.mjs
+++ api/src/app.mjs
@@ -12,9 +12,9 @@ app.get('/', (req, res) => {
         .send('Comic Collection');
 });
 
-app.get('/comics', async(req, res) => {
-    let gremlinQuery = 'g.V().hasLabel("comic")'
+app.get('/comics', async (req, res) => {
     const { search } = req.query;
+    let gremlinQuery = 'g.V().hasLabel("comic")'
     if (search) {
         gremlinQuery += `.has('title', TextP.containing('${search}'))`;
     }
@@ -23,6 +23,48 @@ app.get('/comics', async(req, res) => {
         const comics = await gremlinClient.submit(gremlinQuery, {});
         res.status(200)
             .send(comics);
+    } catch (error) {
+        res.status(400)
+            .send(error);
+    }
+});
+
+app.get('/comics/:id', async (req, res) => {
+    const { id } = req.params;
+    let gremlinQuery = `g.V("${id}")`;
+
+    try {
+        const comic = await gremlinClient.submit(gremlinQuery, {});
+        res.status(200)
+            .send(comic);
+    } catch (error) {
+        res.status(400)
+            .send(error);
+    }
+});
+
+app.get('/comics/:id/sequel-reading', async (req, res) => {
+    const { id } = req.params;
+    let gremlinQuery = `g.V("${id}").out('readingPrequelTo')`;
+
+    try {
+        const comic = await gremlinClient.submit(gremlinQuery, {});
+        res.status(200)
+            .send(comic);
+    } catch (error) {
+        res.status(400)
+            .send(error);
+    }
+});
+
+app.get('/comics/:id/sequel-series', async (req, res) => {
+    const { id } = req.params;
+    let gremlinQuery = `g.V("${id}").out('seriesPrequelTo')`;
+    
+    try {
+        const comic = await gremlinClient.submit(gremlinQuery, {});
+        res.status(200)
+            .send(comic);
     } catch (error) {
         res.status(400)
             .send(error);
