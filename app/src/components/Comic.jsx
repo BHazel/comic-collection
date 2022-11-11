f71db2f5de5dfa780dcf7bbdbb994cca4e8e71b3
Benedict Hazel <bwhazel@gmail.com>
2022-11-11 21:13:19 +0000

    Update comic component to include sequels.

--- app/src/components/Comic.jsx
+++ app/src/components/Comic.jsx
@@ -1,20 +1,35 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
-import { getComic } from '../services/comicService';
+import { getComic, getSequel } from '../services/comicService';
 
 const Comic = () => {
     const { id } = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [comic, setComic] = useState({});
+    const [sequelReading, setSequelReading] = useState(false);
+    const [sequelSeries, setSequelSeries] = useState(false);
+
+    function renderSequelLink(sequel) {
+        if (Object.keys(sequel).length > 0) {
+            return <Link to={`/comics/${sequel.id}`}>{sequel.title}</Link>
+        } else {
+            return 'No sequel.'
+        }
+    }
 
     useEffect(() => {
-        getComic(id)
-            .then(comic => {
-                setComic(comic);
-                setIsLoading(false);
-            });
-    }, [isLoading]);
+        Promise.all([
+            getComic(id),
+            getSequel(id, 'series').catch(error => 'error'),
+            getSequel(id, 'reading').catch(error => 'error')
+        ]).then(responses => {
+            setComic(responses[0]);
+            setSequelSeries(responses[1] !== 'error' ? responses[1] : false);
+            setSequelReading(responses[2] !== 'error' ? responses[2] : false);
+            setIsLoading(false);
+        });
+    }, [id, isLoading]);
 
     if (isLoading) {
         return (
@@ -33,6 +48,11 @@ const Comic = () => {
                     <p><strong>Story:</strong> {comic.storyTitle}</p>
                     <p><strong>Issue: </strong> {comic.issue}</p>
                     <p><strong>Summary: </strong>{comic.story ? comic.story : "No Summary"}</p>
+                    <p><strong>Sequel:</strong></p>
+                    <ul>
+                        <li><strong>Series: </strong>{renderSequelLink(sequelSeries)}</li>
+                        <li><strong>Reading: </strong>{renderSequelLink(sequelReading)}</li>
+                    </ul>
                 </div>
 
                 <div className='col'>
