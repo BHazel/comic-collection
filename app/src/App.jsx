64e57a42a2b67871190c67fd46ff357355202c2c
Benedict Hazel <bwhazel@gmail.com>
2022-11-11 22:33:33 +0000

    Create a navigation component and add to the application router.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Comic from './pages/Comic';
 import Comics from './pages/Comics';
+import Nav from './components/Nav';
 
 const App = () => {
     return (
         <>
             <Router>
+                <Nav />
                 <Routes>
                     <Route path='/comics' element={<Comics />} />
                     <Route path='/comics/:id' element={<Comic />} />
