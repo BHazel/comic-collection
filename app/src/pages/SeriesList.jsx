527f29a4792f12bc2091067a59f570cde58322be
Benedict Hazel <bwhazel@gmail.com>
2022-11-11 23:51:12 +0000

    Update series page to display all series as a table.

--- app/src/pages/SeriesList.jsx
+++ app/src/pages/SeriesList.jsx
@@ -1,7 +1,26 @@
+import { useGlobalContext } from '../context';
+
 const SeriesList = () => {
+    const { series } = useGlobalContext();
+
     return (
         <main className='container'>
             <h1>Series</h1>
+            <p><strong>Count:</strong> {series.length}</p>
+            <table className='table'>
+                <thead>
+                    <th scope='col'>Title</th>
+                </thead>
+                <tbody>
+                    {series.map(series => {
+                        return (
+                            <tr>
+                                <td>{series.title}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </main>
     );
 };
