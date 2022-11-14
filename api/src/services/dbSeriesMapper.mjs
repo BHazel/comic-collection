bb3133a806731d081aaee6536a7b5dd4028b771f
Benedict Hazel <bwhazel@gmail.com>
2022-11-14 00:34:27 +0000

    Add series and image URLs to API mapper.

--- api/src/services/dbSeriesMapper.mjs
+++ api/src/services/dbSeriesMapper.mjs
@@ -1,7 +1,9 @@
 function mapDbSeries(dbSeries) {
     let series = {
         id: dbSeries.id,
-        title: dbSeries.properties.title[0].value
+        title: dbSeries.properties.title[0].value,
+        url: dbSeries.properties.url[0].value,
+        imageUrl: dbSeries.properties.imageUrl[0].value
     };
 
     return series;
